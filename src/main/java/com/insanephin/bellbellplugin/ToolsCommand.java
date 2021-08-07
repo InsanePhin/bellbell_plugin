4132b39f9714a914dd0e42e1ff138f2a6d200f05
Minseo Seo <insanephin@gmail.com>
2021-08-08 01:27:39 +0900

    Update ToolsCommand.java

--- src/main/java/com/insanephin/bellbellplugin/ToolsCommand.java
+++ src/main/java/com/insanephin/bellbellplugin/ToolsCommand.java
@@ -1,10 +1,13 @@
 package com.insanephin.bellbellplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -18,15 +21,19 @@ public class ToolsCommand implements CommandExecutor {
             ItemStack pickaxe = new ItemStack(Material.STONE_PICKAXE);
             ItemStack axe = new ItemStack(Material.STONE_AXE);
 
+            sword.addEnchantment(Enchantment.DURABILITY, 1);
+            pickaxe.addEnchantment(Enchantment.DURABILITY, 1);
+            axe.addEnchantment(Enchantment.DURABILITY, 1);
+
             player.getInventory().addItem(sword, pickaxe, axe);
 
-            player.sendMessage("Complete");
+            player.sendMessage(ChatColor.GREEN+"너에게 내구성1짜리 돌셋을 줄게.");
             return true;
         }
         else if(sender instanceof ConsoleCommandSender) {
-            sender.sendMessage("You can't use this command in console");
+            sender.sendMessage(Color.RED+"You can't use this command in console");
             return false;
         }
-    return false;
+        return false;
     }
 }
