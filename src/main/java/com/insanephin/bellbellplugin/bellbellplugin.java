928ee113822fef1c1c80fcdd018843564eed65a8
Minseo Seo <insanephin@gmail.com>
2021-08-08 01:27:35 +0900

    Update bellbellplugin.java

--- src/main/java/com/insanephin/bellbellplugin/bellbellplugin.java
+++ src/main/java/com/insanephin/bellbellplugin/bellbellplugin.java
@@ -2,20 +2,26 @@ package com.insanephin.bellbellplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.*;
+
 public class bellbellplugin extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        getLogger().info("뭔가 웅장한 브금과 함께 플러그인 [활성화]");
         getCommand("tools").setExecutor(new ToolsCommand());
         getCommand("spawn").setExecutor(new SpawnCommand());
         getCommand("b").setExecutor(new BackCommand());
+
         getServer().getPluginManager().registerEvents(new BackCommand(), this);
 
         getServer().dispatchCommand(getServer().getConsoleSender(), "gamerule showDeathMessages false");
+
+        getLogger().info(Color.GREEN+"벨벨플러그인 [활성화]");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("뭔가 웅장한 브금과 함께 플러그인 [비활성화]");
+        getLogger().info(Color.RED+"벨벨플러그인 [비활성화]");
     }
+
 }
