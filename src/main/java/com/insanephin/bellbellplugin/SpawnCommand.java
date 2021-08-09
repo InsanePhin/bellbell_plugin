52566988fc7c53293c9131c3f55e31f347e15d09
Minseo Seo <insanephin@gmail.com>
2021-08-09 14:30:52 +0900

    Fix ChatColor

--- src/main/java/com/insanephin/bellbellplugin/SpawnCommand.java
+++ src/main/java/com/insanephin/bellbellplugin/SpawnCommand.java
@@ -16,7 +16,7 @@ public class SpawnCommand implements CommandExecutor {
 
             player.teleport(player.getBedSpawnLocation());
 
-            player.sendMessage(ChatColor.MAGIC+"여기가 너의 스폰지역이야.");
+            player.sendMessage(ChatColor.GREEN+"스폰지역으로 이동하였습니다.");
             return true;
         }
         else if(sender instanceof ConsoleCommandSender) {
