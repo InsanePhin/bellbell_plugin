629ad80b56b37458b949d5e3ddb68e12203496c7
Minseo Seo <insanephin@gmail.com>
2021-08-09 14:29:21 +0900

    Update Bellbellplugin.java

--- src/main/java/com/insanephin/bellbellplugin/Bellbellplugin.java
+++ src/main/java/com/insanephin/bellbellplugin/Bellbellplugin.java
@@ -4,17 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.awt.*;
 
-public class Bellbellplugin extends JavaPlugin {
+public class bellbellplugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getCommand("tools").setExecutor(new ToolsCommand());
         getCommand("spawn").setExecutor(new SpawnCommand());
         getCommand("b").setExecutor(new BackCommand());
+        getCommand("tpa").setExecutor(new TpaCommand());
+        getCommand("tpaccept").setExecutor(new TpacceptCommand());
+        getCommand("tpadeny").setExecutor(new TpadenyCommand());
 
         getServer().getPluginManager().registerEvents(new BackCommand(), this);
 
         getServer().dispatchCommand(getServer().getConsoleSender(), "gamerule showDeathMessages false");
+
         getLogger().info(Color.GREEN+"벨벨플러그인 [활성화]");
     }
 
